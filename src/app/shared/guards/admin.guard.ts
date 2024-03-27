8ac9d82c873ddbd36de383cd42b608edf63e0539
Guillaume Cometto <guillaume.cometto@sncf.fr>
2024-03-27 10:52:47 +0100

    ok

--- src/app/shared/guards/admin.guard.ts
+++ src/app/shared/guards/admin.guard.ts
@@ -1,19 +1,12 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Route,
-  Router,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AdminGuard implements CanActivate {
+export class AdminGuard  {
   constructor(private router: Router, public authService: AuthService) {}
 
   canActivate(
